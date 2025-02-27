70c18466675efb2d0d2e616cc0e9fe46afd5a4b9
DamasoMagno <limamdamaso@gmail.com>
2025-02-26 21:52:17 -0300

    feat: add posts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,12 +6,45 @@ const materias = [
   "Português",
   "História",
   "Geografia",
-  "Ciências",
+  // "Ciências",
+];
+
+const posts = [
+  {
+    id: 1,
+    titulo: "🌍 Descubra os Segredos da Geografia!",
+    descricao:
+      "Conheça os principais conceitos de Geografia de forma clara e prática. Entenda mapas, relevo, clima e muito mais!",
+  },
+  {
+    id: 2,
+    titulo: "📖 Português Sem Mistérios: Dicas Essenciais!",
+    descricao:
+      "Aprenda gramática, interpretação de texto e redação com explicações simples e diretas. Torne-se um mestre da língua portuguesa!",
+  },
+  {
+    id: 3,
+    titulo: "🏛 História: Momentos que Moldaram o Mundo!",
+    descricao:
+      "Reviva os principais acontecimentos históricos e entenda como eles impactam o presente. Uma viagem pelo passado!",
+  },
+  {
+    id: 4,
+    titulo: "🔬 Ciências Descomplicadas: Aprenda com Facilidade!",
+    descricao:
+      "Quer entender os principais conceitos de Ciências? Neste resumo, exploramos Biologia, Química e Física de forma simples e objetiva, facilitando o aprendizado e a aplicação no dia a dia. 🚀🔬",
+  },
+  {
+    id: 5,
+    titulo: "💰 Educação Financeira: Como Cuidar do Seu Dinheiro!",
+    descricao:
+      "Aprenda a organizar suas finanças, economizar e investir de forma inteligente. Um guia essencial para sua vida financeira!",
+  },
 ];
 
 export default function Home() {
   return (
-    <div>
+    <div className="px-8">
       <header className="max-w-[800px] mx-auto">
         <h3 className="flex items-center justify-center gap-2 mt-4 font-bold text-xl">
           <Book size={16} />
@@ -23,58 +56,28 @@ export default function Home() {
             <Menu color="#333" size={20} />
           </button>
           {materias.map((materia, index) => (
-            <Link href={`/materia/${materia.toLowerCase()}`} key={index}>
+            <Link href="/" key={index}>
               {materia}
             </Link>
           ))}
         </nav>
       </header>
 
-      <main>
-        <ul className="flex flex-col gap-4">
-          <Link href="/">
-            <strong>
-              📚 Domine a Matemática: Conceitos Essenciais para Aprender de
-              Verdade!
-            </strong>
-            <p>
-              Quer melhorar seu desempenho em Matemática? Neste resumo,
-              abordamos os conceitos fundamentais de forma simples e objetiva,
-              ajudando você a entender e aplicar no dia a dia. 🚀🔢
-            </p>
+      <main className="max-w-[800px] mx-auto mt-16">
+        <ul className="flex flex-col gap-8">
+          {posts.map((post) => {
+            return (
+              <Link href="/" className="flex flex-col gap-2" key={post.id}>
+                <strong>{post.titulo}</strong>
+                <p className="text-gray-500">{post.descricao}</p>
 
-            <footer>
-              <span>Matemática</span>
-              <span>Texto</span>
-            </footer>
-          </Link>
-
-          <Link href="/">
-            <strong>🌍 Descubra os Segredos da Geografia!</strong>
-            <p>
-              Conheça os principais conceitos de Geografia de forma clara e
-              prática. Entenda mapas, relevo, clima e muito mais!
-            </p>
-
-            <footer>
-              <span>MMatemática</span>
-              <span>Texto</span>
-            </footer>
-          </Link>
-
-          <Link href="/">
-            <strong>📖 Português Sem Mistérios: Dicas Essenciais!</strong>
-            <p>
-              Aprenda gramática, interpretação de texto e redação com
-              explicações simples e diretas. Torne-se um mestre da língua
-              portuguesa!
-            </p>
-
-            <footer>
-              <span>MMatemática</span>
-              <span>Texto</span>
-            </footer>
-          </Link>
+                <footer className="flex justify-between items-center">
+                  <span>Matemática</span>
+                  <span>Texto</span>
+                </footer>
+              </Link>
+            );
+          })}
         </ul>
       </main>
     </div>
