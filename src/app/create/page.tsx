6f7e8addd05d95ad0741fa6256e8271bc1f33eb8
DamasoMagno <limamdamaso@gmail.com>
2025-03-01 16:29:20 -0300

    fix: commented unused variables

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -2,7 +2,7 @@
 import { Book } from "lucide-react";
 // import { useForm } from "react-hook-form";
 // import { zodResolver } from "@hookform/resolvers/zod";
-import { z } from "zod";
+// import { z } from "zod";
 
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
